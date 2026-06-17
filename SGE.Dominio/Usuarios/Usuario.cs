eba54fde42a4ef87e7c05facc9cfc5df6be99739
nauelo <eliashuel@yahoo.com.ar>
2026-06-17 17:08:01 -0300

    .

--- SGE.Dominio/Usuarios/Usuario.cs
+++ SGE.Dominio/Usuarios/Usuario.cs
@@ -1,5 +1,62 @@
-namespace SGE.Dominio.Expedientes;
+using SGE.Dominio.Comun;
+
+namespace SGE.Dominio.Usuarios;
 
 public class Usuario
 {
+    public Guid Id {get; private set;}
+    public String Nombre {get; private set;}
+    public CorreoElectronicoVO CorreoElectronico {get; private set;}
+    public String ContrasenaHash {get; private set;}
+    public bool EsAdministrador {get; private set;}
+    public List<String> Permisos {get; private set;}
+
+
+    public Usuario (String nombre, CorreoElectronicoVO correoElectronico, String contrasenaHash, List<String> permisos)
+    {
+        if (string.IsNullOrEmpty(nombre))
+            throw new DominioException ("El campo nombre no puede estar vacio");  
+
+        if (string.IsNullOrEmpty(contrasenaHash))
+        {
+            throw new DominioException ("El campo contraseña no puede estar vacio");  
+        }
+
+        Id= new Guid();
+        Nombre= nombre;
+        CorreoElectronico= correoElectronico;
+        ContrasenaHash=contrasenaHash;
+        EsAdministrador=false;
+        Permisos= new List<string> ();
+
+    }
+
+    protected Usuario(){}
+
+
+    public void ModificarNombre(string nombre)
+    {
+        if (string.IsNullOrWhiteSpace(nombre))
+        {
+            throw new DominioException ("El nombre no puede estar vacio");  
+        }
+
+        Nombre=nombre;
+    }
+
+    public void ModificarCorreoElectronico(CorreoElectronicoVO correoElectronico)
+    {
+        CorreoElectronico= correoElectronico;
+    }
+
+
+    public void ModificarPermiso(List<string> permisos)
+    {
+        Permisos= permisos;
+    }
+
+    public void ModificarAdministrador(bool esAdministrador)
+    {
+        EsAdministrador= esAdministrador;
+    }
 }
